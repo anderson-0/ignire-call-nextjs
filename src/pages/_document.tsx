f67ede1e45f7e0c17aec77713e08cb48ea161c5d
Anderson Silva <60013836+anderson-0@users.noreply.github.com>
2023-06-03 15:15:41 -0400

    chore: eslint config and fixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,10 +4,20 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Montserrat:ital,wght@0,700;1,400&family=Poppins:wght@400;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
-        <style id='stitches' dangerouslySetInnerHTML={{ __html: getCssText() }} />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Montserrat:ital,wght@0,700;1,400&family=Poppins:wght@400;700&family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
       </Head>
       <body>
         <Main />
