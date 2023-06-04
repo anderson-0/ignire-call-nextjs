3dfd0e9d28cf3e0c108787f4d89178278d7a7e34
Anderson Silva <60013836+anderson-0@users.noreply.github.com>
2023-06-04 09:33:19 -0400

    feat: ClaimUsernameForm component

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,18 +1,54 @@
-import { Button, TextInput } from '@ignite-ui/react'
-import { Form } from './styles'
+import { Button, TextInput, Text } from '@ignite-ui/react'
+import { Form, FormAnnotation } from './styles'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O nome de usuário deve ter pelo menos 3 caracteres' })
+    .max(20, { message: 'O nome de usuário deve ter no máximo 20 caracteres' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O nome de usuário deve conter apenas letras e traços',
+    }),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
 export function ClaimUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
-      <TextInput
-        size="sm"
-        prefix="ignite.com/"
-        placeholder="Digite seu nome de usuário"
-      />
-      <Button size="sm" type="submit">
-        Reivindicar
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="Digite seu nome de usuário"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Reivindicar
+          <ArrowRight />
+        </Button>
+        {errors.username && (
+          <FormAnnotation>
+            <Text size="sm">{errors.username?.message}</Text>
+          </FormAnnotation>
+        )}
+      </Form>
+    </>
   )
 }
